09f47b6099d4d57f613ed21203fd9934475c9307
MAkzent <arnold.michael91@gmail.com>
2018-08-23 10:03:02 +0900

    added env variable

--- src/components/TedTalk.vue
+++ src/components/TedTalk.vue
@@ -36,7 +36,7 @@ methods: {
   },
   getAPIKey: function () {
     if (process.env === "development") {
-      this.youtubeAPIKey = youtubeAPIKey;
+      this.youtubeAPIKey = prcoess.env.VUE_APP_YT;
     } else if (process.env === "production"){
       this.youtubeAPIKey = process.env.YT_KEY
     }
